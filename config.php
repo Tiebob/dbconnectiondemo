39daba5079290904ae2fd5698a00b5e80fdf3d78
bob <tiebob@gmail.com>
2015-09-16 11:23:02 +0800

    資料庫及連線資訊(主機、帳號、密碼)

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bob
+ * Date: 2015/9/16
+ * Time: 上午 08:57
+ */
+
+$host   = 'localhost';
+$user   = 'root';
+$pass   = 'mysql';
+$dbname = 'test';
---
+++ db_test.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Community v12.08 (64 bit)
+MySQL - 5.6.24 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `test`;
+
+/*Table structure for table `contact` */
+
+DROP TABLE IF EXISTS `contact`;
+
+CREATE TABLE `contact` (
+  `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `ename` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `cname` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `phone` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `contact` */
+
+insert  into `contact`(`id`,`ename`,`cname`,`phone`,`address`,`created_time`) values (1,NULL,'黃小明','0226812011','新北市樹林區育英街176號','2015-09-13 12:02:59'),(2,NULL,'王小華','0226823331','新北市樹林區育英街176號','2015-09-13 12:02:59'),(3,NULL,'陳玉雪','0226828737','新北市樹林區中山路一段166號','2015-09-13 12:02:59');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
